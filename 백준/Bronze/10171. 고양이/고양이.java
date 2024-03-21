47b98f4d2e22a61cceaf3562c1fb615caeb400a2
Minseok Kim <mskim.glenn@gmail.com>
2024-03-21 11:05:46 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       // 1. Print cat
+        printCat();
+    }
+
+    public static void printCat() {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
