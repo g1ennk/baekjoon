f387ffa833ea0cf4b07c51abd1dfb3698e999a5f
Minseok Kim <mskim.glenn@gmail.com>
2024-03-21 11:08:46 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       // 1. Print dog
+        printDog();
+    }
+
+    public static void printDog() {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|\n");
+    }
+}
