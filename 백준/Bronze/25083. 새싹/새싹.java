06de7b5c0152e35de0f8f302e1d93493b2305421
Minseok Kim <mskim.glenn@gmail.com>
2024-03-26 09:35:37 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Print sprout
+        printSprout();
+    }
+
+    // Method to print sprout
+    public static void printSprout() {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
