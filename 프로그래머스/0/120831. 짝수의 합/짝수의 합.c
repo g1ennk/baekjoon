1946a994be9207003f4918f83056abfe55fc48c1
Minseok Kim <mskim.glenn@gmail.com>
2024-05-09 17:08:31 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    // 초기화
+    int answer = 0;
+    int sum = 0;
+    
+    // 더하기
+    for (int i = 0; i <= n; i += 2) {
+        sum += i;
+    }
+    
+    return sum;
+}
