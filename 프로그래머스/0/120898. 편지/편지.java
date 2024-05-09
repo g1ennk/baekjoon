f4c4d92cf6ca0b026c2bc5fd4cc3a74b2ad1087b
Minseok Kim <mskim.glenn@gmail.com>
2024-05-09 17:35:29 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        // 초기화
+        int textLen = 2;
+        int textCount = message.length();
+        
+        return textLen * textCount;
+    }
+}
