95efcad288b55b319d754eec4ea7fed19bd26a11
Minseok Kim <mskim.glenn@gmail.com>
2024-05-13 10:18:47 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,14 +1,7 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        // 새로운 배열의 크기 계산 및 초기화
-        int newSize = num2 - num1 + 1;
-        int[] newNumbers = new int[newSize];
-        
-        // arraycopy를 활용한 슬라이스
-        System.arraycopy(numbers, num1, newNumbers, 0, newSize);
-        
-        return newNumbers;
+        return Arrays.copyOfRange(numbers, num1 , num2 + 1);
     }
 }
