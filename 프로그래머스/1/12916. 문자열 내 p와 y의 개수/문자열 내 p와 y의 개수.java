c449dc6a6f88b4a2ab62e653b69dd1e3304f2a6c
Minseok Kim <mskim.glenn@gmail.com>
2024-05-14 11:43:20 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,27 +1,23 @@
 class Solution {
     boolean solution(String s) {
+        // 대소문자 구분 없이 비교하기 위해 입력 문자열을 모두 대문자로 변환
+        s = s.toUpperCase();
         
-        boolean answer = true;
+        // 'P'와 'Y'의 개수를 저장할 변수 초기화
         int pCount = 0;
         int yCount = 0;
 
-        // 문자열을 대문자로 변환하여 다시 저장
-        s = s.toUpperCase();
-        
-        for(int i = 0; i < s.length(); i++) {
-            // 문자열의 각 문자를 검사하여 'P'와 'Y'의 개수를 센다
-            if(s.charAt(i) == 'P') {
+        // 문자열의 각 문자를 검사하여 'P'와 'Y'의 개수를 센다
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'P') {
                 pCount++;
-            }
-            
-            if(s.charAt(i) == 'Y') {
+            } else if (ch == 'Y') {
                 yCount++;
             }
         }
         
-        // 'P'와 'Y'의 개수가 같은지 확인하여 answer를 설정
-        answer = (pCount == yCount) ? true : false;
-        
-        return answer;
+        // 'P'와 'Y'의 개수가 같은지 확인하여 결과를 반환
+        return pCount == yCount;
     }
 }
