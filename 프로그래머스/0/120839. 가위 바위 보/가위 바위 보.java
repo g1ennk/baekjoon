df0bfdfa1f1fa8905ffc15e4722bc5945ffe70c5
Minseok Kim <mskim.glenn@gmail.com>
2024-05-16 10:32:01 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,9 +1,10 @@
+// 두 번째 코드 (StringBuilder 사용)
 class Solution {
     public String solution(String rsp) {
         
         // 초기화
         int length = rsp.length();
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
         // 반복문을 돌며 케이스 검사
         for(int i = 0; i < length; i++) {
@@ -11,19 +12,19 @@ class Solution {
             
             switch (c) {
                 case '2':
-                    answer += '0';
+                    answer.append('0');
                     break;
                 
                 case '0':
-                    answer += '5';
+                    answer.append('5');
                     break;
                     
                 case '5':
-                    answer += '2';
+                    answer.append('2');
                     break;
             }
         }
         
-        return answer;
+        return answer.toString();
     }
 }
