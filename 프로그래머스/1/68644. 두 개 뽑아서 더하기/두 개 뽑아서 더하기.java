33a6508d3374d25e6ebe7033279ff9b25228e8f4
Minseok Kim <mskim.glenn@gmail.com>
2024-06-24 11:31:44 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.27 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,19 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        // TreeSet 초기화
+        Set<Integer> sumSet = new TreeSet<>();
+        
+        // 이중 반복문을 통해 두 개의 숫자를 뽑아 더해 sumSet에 저장
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                sumSet.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        // stream -> Integer -> int -> array
+        return sumSet.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
