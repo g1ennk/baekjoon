143c19adb61012551db58e1388664fa45952604a
Minseok Kim <mskim.glenn@gmail.com>
2024-07-09 15:58:55 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 552 ms, Memory: 55416 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,4 +1,3 @@
-//package algorithm.basic.stack;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,19 +6,19 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String []args) throws IOException {
+    public static void main(String[] args) throws IOException {
         // 반복 횟수 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int reps = Integer.parseInt(br.readLine());
         String[] answers = new String[reps];
-        
+
         // 반복 횟수만큼 문장에 있는 단어 뒤집기
         for (int i = 0; i < reps; i++) {
             answers[i] = reverseWords(br.readLine());
         }
-        
+
         // 출력
-        for(String str : answers) {
+        for (String str : answers) {
             System.out.println(str);
         }
 
@@ -27,8 +26,10 @@ public class Main {
 
     public static String reverseWords(String s) {
         String[] words = s.split(" ");
-        StringBuilder sb = new StringBuilder();
 
+
+        /* Stack 활용한 뒤집기
+        StringBuilder sb = new StringBuilder();
         for(String word : words) {
             Stack<Character> stack = new Stack<>();
             for(char ch : word.toCharArray()) {
@@ -39,8 +40,16 @@ public class Main {
                 sb.append(stack.pop());
             }
             sb.append(" ");
-        }
+        } */
 
-        return sb.toString().trim();
+        // StringBuilder 활용한 뒤집기
+        StringJoiner sj = new StringJoiner(" ");
+        for(String word : words) {
+            StringBuilder wordBuilder = new StringBuilder(word);
+            wordBuilder.reverse();
+            sj.add(wordBuilder.toString());
+        }
+        
+        return sj.toString();
     }
 }
