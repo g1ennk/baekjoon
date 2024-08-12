cdaad5c5546ac1b77b090b88b819fd11db4d6397
Minseok Kim <mskim.glenn@gmail.com>
2024-08-12 18:26:13 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,27 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int commandsLen = commands.length;
-        int answer[] = new int[commandsLen];
+        int N = commands.length;
+        int[] answer = new int[N];
         
-        // 1. commands의 길이만큼 반복한다.
-        for(int i = 0; i < commandsLen; i++){
-            int startIdx = commands[i][0] - 1;
-            int endIdx = commands[i][1];
-            int pickIdx = commands[i][2] - 1;
+        for(int i = 0; i < N; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int index = commands[i][2] - 1;
             
-            // 1-1. commands[0]부터 commands[1]까지 배열을 자른다.
-            int[] slicedArray = Arrays.copyOfRange(array, startIdx, endIdx);
-            
-            // 1-2. 자른 배열을 오름차순으로 정렬한다.
-            Arrays.sort(slicedArray);
-            
-            // 1-3. 배열의 commands[2]번째 수를 정답 배열에 저장한다.
-            answer[i] = slicedArray[pickIdx];
+            int[] tempArray = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(tempArray);
+            answer[i] = tempArray[index];
         }
-            
         
-        // 2. 정답 배열을 리턴한다.
         return answer;
     }
 }
