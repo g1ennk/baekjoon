fb2f913f4120bbc40f0cb485b3ca639a570982a1
Minseok Kim <mskim.glenn@gmail.com>
2024-08-14 15:47:15 +0900

    [level 1] Title: 모의고사, Time: 4.13 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,45 +2,45 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1, 2, 3, 4 ,5};
+        // 수포자 정답 패턴
+        int[] first = {1, 2, 3, 4, 5};
         int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third  = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        // 수포자와 점수 매핑
+        // 각각의 수포자와 점수 매핑
         Map<Integer, Integer> scores = new HashMap<>();
-        scores.put(1, getCountAnswers(first, answers));
-        scores.put(2, getCountAnswers(second, answers));
-        scores.put(3, getCountAnswers(third, answers));
+        scores.put(1, getAnswerCount(answers, first));
+        scores.put(2, getAnswerCount(answers, second));
+        scores.put(3, getAnswerCount(answers, third));
         
-        // 가장 높은 점수 찾기
+        // 가장 큰 점수 찾기
         int maxScore = Collections.max(scores.values());
         
-        // 가장 높은 점수를 가진 수포자 찾기
-        List<Integer> bestStudents = new ArrayList<>();
+        // 가장 큰 점수를 가진 수포자 찾기
+        List<Integer> topScorers = new ArrayList<>();
         for(Map.Entry<Integer, Integer> entry : scores.entrySet()) {
             if(entry.getValue() == maxScore) {
-                bestStudents.add(entry.getKey());
+                topScorers.add(entry.getKey());
             }
         }
         
-        // 가장 높은 점수를 가진 수포자가 여러 명일 경우 오름차순 정렬
-        Collections.sort(bestStudents);
-
-        return bestStudents.stream().mapToInt(Integer::intValue).toArray();
+        // 여러 명일 경우 오름차순 정렬
+        Collections.sort(topScorers);
+        
+        // 배열로 바꾼 후 리턴
+        return topScorers.stream().mapToInt(Integer::intValue).toArray();
     }
     
-    public static int getCountAnswers(int[] tries, int[] answers) {
-        int count = 0;
-        for(int i = 0, j = 0; i < answers.length; i++, j++) {
-            if(j == tries.length) {
-                j = 0;
-            }
-            
-            if(answers[i] == tries[j]) {
-                count++;
+    public int getAnswerCount(int[] answers, int[] pattern) {
+        int correctCount = 0;
+        int patternLength = pattern.length;
+        
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == pattern[i % patternLength]) {
+                correctCount++;
             }
         }
         
-        return count;
+        return correctCount;
     }
 }
