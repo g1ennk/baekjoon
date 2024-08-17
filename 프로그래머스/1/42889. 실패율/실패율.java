7b6320a8bf7e870f8c84732a3ddb2dd11bb22722
Minseok Kim <mskim.glenn@gmail.com>
2024-08-17 16:25:59 +0900

    [level 1] Title: 실패율, Time: 438.52 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,30 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        Map<Integer, Double> failRates = new HashMap<>();
-        int challengers = stages.length;
-        
-        // 실패율 계산
-        for(int i = 1; i <= N; i++) {
-            int count = 0;
-            for(int j = 0; j < stages.length; j++) {
-                if(i == stages[j]) {
-                    count++;
+        double[] failRates = new double[N];
+        Integer[] stageIndexes = new Integer[N];
+
+        // 1. 실패율 계산
+        for (int i = 1; i <= N; i++) {
+            int playersReached = 0;
+            int playersStuck = 0;
+
+            // 각 스테이지에 대한 통계 계산
+            for (int stage: stages) {
+                if (stage >= i) {
+                    playersReached++;
+                }
+                if (stage == i) {
+                    playersStuck++;
                 }
             }
-            if(challengers > 0) {
-                failRates.put(i, (double) count / challengers);
-            } else {
-                failRates.put(i, 0.0);
-            }
-            challengers -= count;
+
+            // 실패율 계산
+            failRates[i - 1] = (playersReached == 0) ? 0 : (double) playersStuck / playersReached;
+            stageIndexes[i - 1] = i;
         }
-        
-        // 실패율이 높은 것부터 내림차순으로 정렬, 만약 실패율이 같다면 작은 key 값을 먼저
-        return failRates
-            .entrySet()
-            .stream()
-            .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
-            .mapToInt(HashMap.Entry::getKey).toArray();
+
+        // 2. 실패율을 기준으로 스테이지 인덱스 정렬
+        Arrays.sort(stageIndexes, (a, b) 
+                    // 같은 경우 번호를 가지고 오름차순 정렬
+                    -> Double.compare(failRates[b - 1], failRates[a - 1]) == 0 
+                    ? a - b 
+                    // 아닌 경우 실패율을 가지고 내림차순 정렬
+                    : Double.compare(failRates[b - 1], failRates[a - 1])); 
+
+        // 3. 정렬된 스테이지 인덱스를 결과로 반환
+        return Arrays.stream(stageIndexes).mapToInt(Integer::intValue).toArray();
     }
 }
