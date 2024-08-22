6950ec8c106b8d31e40150803cd854744b2d3139
Minseok Kim <mskim.glenn@gmail.com>
2024-08-22 17:29:09 +0900

    [level 2] Title: 기능개발, Time: 17.45 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
-        int N = progresses.length;
+        List <Integer> list = new ArrayList<>();
+        Queue <Integer> queue = new ArrayDeque<>();
         
-        // 1. 각 작업에 필요한 날짜를 계산한 후 큐에 삽입
-        for(int i = 0; i < N; i++) {
-            int daysRequired = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            queue.offer(daysRequired);
-        }
-        
-        // 2. 모든 작업이 끝날 때까지 배포 가능한 작업 그룹을 계산
-        while(!queue.isEmpty()) {
-            int count = 1;
-            int target = queue.poll();
+        // 1. 배열을 돌면서 작업 기간 계산하기
+        for(int i = 0; i < progresses.length; i++) {
+            // 1-1. 계산 ((100 - progress) / speed)
+            int requiredDay = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
             
-            while(!queue.isEmpty() && queue.peek() <= target) {
+            // 1-2. 큐에 넣기
+            queue.offer(requiredDay);
+        }
+           
+        // 2. 큐를 하나씩 빼면서 처음 작업을 기준으로 작다면 카운트 올리고, 크다면 리스트에 추가 후 카운트 초기화
+        while(!queue.isEmpty()) {
+            int standard = queue.poll();
+            int count = 1;
+            
+            while(!queue.isEmpty() && queue.peek() <= standard) {
                 count++;
                 queue.poll();
             }
-            
             list.add(count);
+            
         }
         
-        // 리스트를 배열로 변환
-        return list.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        // 3. 리스트를 배열로 변환하여 리턴
+        return list.stream().mapToInt(Integer :: intValue).toArray();
     }
 }
