d53d30c9bc51993c2467039d90318c6380f7f7b7
Minseok Kim <mskim.glenn@gmail.com>
2024-08-22 16:11:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.64 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,21 +2,15 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        List<Integer> list = new ArrayList<>();
+        List <Integer> list = new ArrayList<>();
         
-        for(int i = 0; i < arr.length; i++) {
-            if(i != arr.length - 1 && arr[i] == arr[i + 1]) {
-                continue;
-            } else {
+        list.add(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[i - 1]) {
                 list.add(arr[i]);
-            }
+            } 
         }
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        return list.stream().mapToInt(Integer :: intValue).toArray();
     }
 }
