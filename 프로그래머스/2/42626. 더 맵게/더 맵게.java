877f872eb1cf424b839a630f612fa44b2832155a
Minseok Kim <mskim.glenn@gmail.com>
2024-08-31 13:04:06 +0900

    [level 2] Title: 더 맵게, Time: 1639.32 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,22 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        PriorityQueue <Integer> pQ = new PriorityQueue<>();
         int count = 0;
-        
-        for(int s : scoville) {
-            pQ.offer(s);
+
+        // 1. 우선순위 큐에 집어넣기
+        for(int scov : scoville) {
+            pQ.add(scov);
         }
         
-        while(pQ.size() >= 2 && pQ.peek() < K) {
+        // 2. 큐를 돌면서 K보다 작은 게 없을 때까지 섞기 (없다면)
+        while (pQ.size() > 1 && pQ.peek() < K) {
             int first = pQ.poll();
             int second = pQ.poll();
-            int newOne = first + (second * 2);
-            
-            pQ.offer(newOne);
+            pQ.add(first + (second * 2));
             count++;
         }
         
+        if(pQ.peek() < K) {
+            return -1;
+        }
+        
+        // 3. 섞어야 하는 횟수 리턴하기
         return (pQ.peek() < K) ? -1 : count;
     }
 }
