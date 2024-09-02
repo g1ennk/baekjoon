c72fecf76b5b298983226120e028b8e343d6254a
Minseok Kim <mskim.glenn@gmail.com>
2024-09-02 10:45:08 +0900

    [level 1] Title: K번째수, Time: 0.56 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int N = commands.length;
-        int[] answer = new int[N];
+        int[] answer = new int[commands.length];
         
-        for(int i = 0; i < N; i++) {
-            int start = commands[i][0] - 1;
-            int end = commands[i][1];
-            int index = commands[i][2] - 1;
+        for(int i = 0; i < commands.length; i++) {
+            int startIdx = commands[i][0] - 1;
+            int endIdx = commands[i][1];
+            int selectIdx = commands[i][2] - 1;
             
-            int[] tempArray = Arrays.copyOfRange(array, start, end);
-            Arrays.sort(tempArray);
-            answer[i] = tempArray[index];
+            // 1. 자르고(i-1 ~ k)
+            int[] newArr = Arrays.copyOfRange(array, startIdx, endIdx);
+                    
+            // 2. 정렬하고(오름차순)
+            Arrays.sort(newArr);
+
+            // 3. k번째 수 뽑기
+            answer[i] = newArr[selectIdx];
         }
         
         return answer;
