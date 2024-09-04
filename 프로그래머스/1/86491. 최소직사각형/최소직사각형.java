8b6cf5a5d958279a970e190b72a29214c47ecc76
Minseok Kim <mskim.glenn@gmail.com>
2024-09-05 08:52:07 +0900

    [level 1] Title: 최소직사각형, Time: 1.22 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,32 +1,29 @@
 class Solution {
     public int solution(int[][] sizes) {
-        // 1. x가 y중 작은 값을 앞으로 스왑
+        int maxWidth = 0;
+        int maxHeight = 0;
+        
         for(int i = 0; i < sizes.length; i++) {
-            if(sizes[i][0] > sizes[i][1]) {
+            // 세로가 가로보다 크다면 swap
+            if(sizes[i][0] < sizes[i][1]) {
                 swap(sizes[i]);
             }
-        }
-        
-        // 2. 하나씩 돌면서 x중 가장 큰 값과 y중 가장 큰 값을 찾기
-        int maxWidth = sizes[0][0];
-        int maxHeight = sizes[0][1];
-        for(int i = 1; i < sizes.length; i++) {
+            
+            // 가로 및 길이 최댓값 찾기
             if(sizes[i][0] > maxWidth) {
                 maxWidth = sizes[i][0];
             }
-            
             if(sizes[i][1] > maxHeight) {
                 maxHeight = sizes[i][1];
             }
         }
         
-        // 3. 그 둘을 곱해서 반환
         return maxWidth * maxHeight;
     }
     
-    public static void swap(int[] xY) {
-        int temp = xY[0];
-        xY[0] = xY[1];
-        xY[1] = temp;
+    public void swap (int[] sizes) {
+        int temp = sizes[0];
+        sizes[0] = sizes[1];
+        sizes[1] = temp;
     }
 }
