a3200ee6dfb1208e5df0eccc3111964b70bcd98e
Minseok Kim <mskim.glenn@gmail.com>
2024-09-06 10:34:41 +0900

    [level 1] Title: 모의고사, Time: 4.65 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,45 +2,44 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        // 수포자 정답 패턴
-        int[] first = {1, 2, 3, 4, 5};
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        // 수포자 패턴
+        int[] firstPattern = {1, 2, 3, 4, 5};
+        int[] secondPattern = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] thirdPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        // 각각의 수포자와 점수 매핑
-        Map<Integer, Integer> scores = new HashMap<>();
-        scores.put(1, getAnswerCount(answers, first));
-        scores.put(2, getAnswerCount(answers, second));
-        scores.put(3, getAnswerCount(answers, third));
+        // 각각의 수포자들의 정답 배열
+        int[] countArr = new int[3];
+        countArr[0] = getCount(firstPattern, answers);
+        countArr[1] = getCount(secondPattern, answers);
+        countArr[2] = getCount(thirdPattern, answers);
         
-        // 가장 큰 점수 찾기
-        int maxScore = Collections.max(scores.values());
-        
-        // 가장 큰 점수를 가진 수포자 찾기
-        List<Integer> topScorers = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> entry : scores.entrySet()) {
-            if(entry.getValue() == maxScore) {
-                topScorers.add(entry.getKey());
+        // 순회하면서 가장 높은 점수를 가진 수포자 구하기(가장 높은 점수를 가진 수포자가 여러 명일 때도 추가)
+        List<Integer> bestStudentList = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < countArr.length; i++) {
+            if (countArr[i] > max) {
+                max = countArr[i];
+                bestStudentList.clear();
             }
+            
+            if (countArr[i] == max) {
+                bestStudentList.add(i + 1);
+            }
+            
         }
-        
-        // 여러 명일 경우 오름차순 정렬
-        Collections.sort(topScorers);
-        
-        // 배열로 바꾼 후 리턴
-        return topScorers.stream().mapToInt(Integer::intValue).toArray();
+
+        return bestStudentList.stream().mapToInt(Integer::intValue).toArray();
     }
     
-    public int getAnswerCount(int[] answers, int[] pattern) {
-        int correctCount = 0;
-        int patternLength = pattern.length;
-        
-        for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == pattern[i % patternLength]) {
-                correctCount++;
+    // 패턴에 맞는 정답 구하기
+    public int getCount(int[] pattern, int[] answers) {
+        int count = 0;
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == pattern[i % pattern.length]) {
+                count++;
             }
         }
-        
-        return correctCount;
-    }
+
+        return count;
+    }   
 }
