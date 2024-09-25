1ba7e195b88f8ecaecea303130d4c54a222e37a2
Minseok Kim <mskim.glenn@gmail.com>
2024-09-25 20:41:33 +0900

    [Silver IV] Title: 듣보잡, Time: 264 ms, Memory: 26272 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,30 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 // 듣보잡
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        HashMap<String, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         // 1. 듣도 못한 사람의 N과 보도 못한 사람의 수 M 입력받기
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-        scanner.nextLine();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        // 2. 듣도 못한 사람의 수와 보도 못한 사람의 수 입력받기
-        for (int i = 0; i < N + M; i++) {
-            String name = scanner.nextLine();
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        // 2. 듣도 못한 사람 입력받기
+        HashSet<String> unheard = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            unheard.add(br.readLine());
         }
 
-        // 3. 값이 2라면 듣도 보도 못한 사람
-        int count = 0;
+        // 듣도 보도 못한 사람 찾기 (교집합 찾기)
         List<String> list = new ArrayList<>();
-
-        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
-            if (stringIntegerEntry.getValue() == 2) {
-                list.add(stringIntegerEntry.getKey());
-                count++;
+        for (int i = 0; i < M; i++) {
+            String unseen = br.readLine();
+            if (unheard.contains(unseen)) {
+                list.add(unseen);
             }
         }
 
@@ -32,11 +32,9 @@ public class Main {
         Collections.sort(list);
 
         // 5. 출력하기
-        System.out.println(count);
+        System.out.println(list.size());
         for (String s : list) {
             System.out.println(s);
         }
-        
-        scanner.close();
     }
 }
