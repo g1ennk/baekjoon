782d0ee2f8e0939d65f57d517821c1685fc7301b
Minseok Kim <mskim.glenn@gmail.com>
2024-09-26 11:34:22 +0900

    [level 1] Title: 모의고사, Time: 3.41 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,45 +1,37 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        // 수포자 패턴
-        int[] firstPattern = {1, 2, 3, 4, 5};
-        int[] secondPattern = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] thirdPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        // 각각의 수포자들의 정답 배열
-        int[] countArr = new int[3];
-        countArr[0] = getCount(firstPattern, answers);
-        countArr[1] = getCount(secondPattern, answers);
-        countArr[2] = getCount(thirdPattern, answers);
-        
-        // 순회하면서 가장 높은 점수를 가진 수포자 구하기(가장 높은 점수를 가진 수포자가 여러 명일 때도 추가)
-        List<Integer> bestStudentList = new ArrayList<>();
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < countArr.length; i++) {
-            if (countArr[i] > max) {
-                max = countArr[i];
-                bestStudentList.clear();
-            }
-            
-            if (countArr[i] == max) {
-                bestStudentList.add(i + 1);
-            }
-            
-        }
-
-        return bestStudentList.stream().mapToInt(Integer::intValue).toArray();
-    }
+    static final int[][] PATTERNS = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
     
-    // 패턴에 맞는 정답 구하기
-    public int getCount(int[] pattern, int[] answers) {
-        int count = 0;
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == pattern[i % pattern.length]) {
-                count++;
+    public int[] solution(int[] answers) {
+        int maxScore = Integer.MIN_VALUE;
+        List<Integer> topScorers = new ArrayList<>();
+
+        for (int i = 0; i < PATTERNS.length; i++) {
+            // 1. 스코어 계산
+            int score = 0;
+            for (int j = 0; j < answers.length; j++) {
+                if (answers[j] == PATTERNS[i][j % PATTERNS[i].length]) {
+                    score++;
+                }
             }
+
+            // 2. 점수 갱신 및 최고득점자 갱신
+            if (score > maxScore) {
+                topScorers.clear(); // 기존 최고 득점자 목록 초기화
+                maxScore = score;
+                topScorers.add(i + 1);
+            } else if (score == maxScore) {
+                // 현재 수포자 점수가 최고 점수자와 같다면 목록에 추가
+                topScorers.add(i + 1);
+            }
+
         }
 
-        return count;
-    }   
+        return topScorers.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
