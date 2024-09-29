15e491882a9c8c6ee626313338f6aed9068adf11
Minseok Kim <mskim.glenn@gmail.com>
2024-09-29 09:54:44 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 520 ms, Memory: 55848 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -2,37 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 // 비밀번호 찾기
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<String, String> map = new HashMap<>();
+        HashMap<String, String> passwordMap = new HashMap<>();
 
         // 1. 저장된 사이트 주소의 수 N과 찾으려는 사이트 주소의 수 M 입력받기
-        String[] input1 = br.readLine().split(" ");
-        int numberOfSites = Integer.parseInt(input1[0]);
-        int numberOfTargetsSites = Integer.parseInt(input1[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int numberOfSites = Integer.parseInt(st.nextToken());
+        int numberOfTargetsSites = Integer.parseInt(st.nextToken());
 
         // 2. 저장된 사이트의 주소의 수를 입력받아 해시맵에 추가
         for (int i = 0; i < numberOfSites; i++) {
-            String[] input = br.readLine().split(" ");
-            String sites = input[0];
-            String password = input[1];
+            st = new StringTokenizer(br.readLine());
+            String sites = st.nextToken();
+            String password = st.nextToken();
 
-            map.put(sites, password);
+            passwordMap.put(sites, password);
         }
 
-        // 3. 찾으려는 사이트의 주소를 입력받아 비밀번호 찾기
-        String[] answers = new String[numberOfTargetsSites];
+        // 3. 찾으려는 사이트의 주소를 입력받아 결과 만들기
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numberOfTargetsSites; i++) {
             String targetSite = br.readLine();
-            answers[i] = map.get(targetSite);
+            sb.append(passwordMap.get(targetSite)).append("\n");
         }
 
-        // 4. 비밀번호 출력하기
-        for (String answer : answers) {
-            System.out.println(answer);
-        }
+        // 4. 결과 출력하기
+        System.out.println(sb);
     }
 }
