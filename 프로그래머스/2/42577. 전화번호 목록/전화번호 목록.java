1a68ccf02452219a80c0711073f25c616bdd2422
Minseok Kim <mskim.glenn@gmail.com>
2024-11-06 12:05:22 +0900

    [level 2] Title: 전화번호 목록, Time: 387.72 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,6 @@
-import java.util.*;
-
 class Solution {
-     public static boolean solution(String[] phone_book) {
-        // 1. 전화번호를 길이순으로 정렬
-        Arrays.sort(phone_book);
-        
-        // 2. 인접한 번호들을 비교
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
-            }
-        }
-        
-        // 접두어가 아니면 true 반환
-        return true;
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        return answer;
     }
 }
