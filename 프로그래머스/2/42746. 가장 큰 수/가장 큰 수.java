f122fc5225b5609ec0ae6f8a624766dc126585a3
Minseok Kim <mskim.glenn@gmail.com>
2024-11-07 19:27:00 +0900

    [level 2] Title: 가장 큰 수, Time: 175.60 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,34 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int[] numbers) {
-        int N = numbers.length;
-        String[] strArr = new String[N];
-        
-        // 1. 문자 배열로 전환하기
-        for(int i = 0; i < N; i++) {
-            strArr[i] = String.valueOf(numbers[i]);
+     public static String solution(int[] numbers) {
+        // 1. 정수 배열을 문자열 배열로 변환
+        String[] numbersStr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbersStr[i] = String.valueOf(numbers[i]);
+        }
+
+        // 2. 커스텀으로 정렬
+        Arrays.sort(numbersStr, ((o1, o2) -> (o2 + o1).compareTo(o1 + o2)));
+
+        // 3. 정렬한 배열을 문자열로 붙이기
+        StringBuilder sb = new StringBuilder();
+        for (String numberStr : numbersStr) {
+            sb.append(numberStr);
         }
         
-        // 2. 정렬하기(두 수를 붙이면서 큰 걸 앞으로 정렬)
-        Arrays.sort(strArr, new Comparator<String>() {
-            public int compare(String o1, String o2) {
-                return (o2 + o1).compareTo(o1 + o2);
-            }
-        });
-        
-        // 예외 처리
-        if(strArr[0].equals("0")) {
+        // 4. 예외 처리 (모두 0인 경우)
+        if (sb.charAt(0) == '0') {
             return "0";
         }
-        
-        // 3. 정렬된 문자열 배열을 하나의 문자열로 합치기
-        StringBuilder sb = new StringBuilder();
-        for(String str : strArr) {
-            sb.append(str);
-        }
-        
-        // 4. 리턴
+
         return sb.toString();
     }
 }
