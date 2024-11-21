e672cd7ebcd48a8ac2d95409fc4f50a6b0ffbb19
Minseok Kim <mskim.glenn@gmail.com>
2024-11-21 15:34:54 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,26 +1,17 @@
+// package algorithm_lecture.basic;
+
 import java.util.Scanner;
 
-// 1000번: A+B
-// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// A + b
 public class Main {
     public static void main(String[] args) {
-        // 1. 스캐너 호출
-        Scanner scanner = new Scanner(System.in);
-
-        // 2. 입력 받기
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-
-        // 3. 계산하기
-        int sum = calculateSum(A,B);
-
-        // 4. 출력하기
-        System.out.println(sum);
-
-        // 5. 스캐너 닫기
-        scanner.close();
-    }
-    public static int calculateSum(int a, int b) {
-        return a + b;
+        Scanner sc = new Scanner(System.in);
+        
+        // 1. A, B 입력받기
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        // 2. 더해서 출력
+        System.out.println(A + B);
     }
 }
