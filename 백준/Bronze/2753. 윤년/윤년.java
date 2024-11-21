4f8094cae93972cf466edcb6df4550b277fcafff
Minseok Kim <mskim.glenn@gmail.com>
2024-11-21 15:55:21 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,19 +1,20 @@
+// package algorithm_lecture.basic;
+
 import java.util.Scanner;
 
+// 윤년
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int year = scanner.nextInt();
+        // 1. 연도 입력받기
+        int year = sc.nextInt();
 
-        System.out.println(isLeapYear(year));
-    }
-
-    public static int isLeapYear(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            return 1;
-        } else {
-            return 0;
+        // 2. 윤년인지 아닌지 판단하기
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(1);
+            return;
         }
+        System.out.println(0);
     }
 }
