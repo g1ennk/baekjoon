a010161592d5fd52e38b7118b2a6757f3a5f7399
Minseok Kim <mskim.glenn@gmail.com>
2024-11-23 20:23:58 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 288 ms, Memory: 18312 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,27 +1,30 @@
-// 2438번: 별 찍기 - 2
-/*
-첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
- */
+// package algorithm_lecture.basic;
 
 import java.util.Scanner;
 
+// 별 찍기 - 2
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // 1. 입력 받기
-        int number = scanner.nextInt();
+        int lines = sc.nextInt();
 
-        // 2. 별찍기
-        for (int i = 0; i < number; i++) {
-            for (int j = i + 1; j < number; j++) {
+        // 2. 별 찍기
+        printStar(lines);
+    }
+
+    static void printStar(int lines) {
+        for (int i = 0; i < lines; i++) {
+            for (int j = lines - 1; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < i + 1 ; j++) {
+
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
-            System.out.println("");
+
+            System.out.println();
         }
     }
 }
