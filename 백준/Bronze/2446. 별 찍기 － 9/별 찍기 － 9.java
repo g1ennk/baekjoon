cc7fb9e176ee29358d6c8d8c90cfa4de911b4c63
Minseok Kim <mskim.glenn@gmail.com>
2024-11-24 21:15:38 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 416 ms, Memory: 21708 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,44 @@
+// package algorithm_lecture.basic;
+
+import java.util.Scanner;
+
+// 별 찍기 - 9
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 1. 입력 받기
+        int lines = sc.nextInt();
+
+        // 2. 별 찍기
+        printStar(lines);
+    }
+
+    static void printStar(int lines) {
+        for (int i = 0; i < lines - 1; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = lines * 2 - 1; j > i * 2; j--) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < lines; i++) {
+
+            for (int j = lines - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i * 2  + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
