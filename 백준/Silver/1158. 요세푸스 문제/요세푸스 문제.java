7751aa043790a9a21531d295d0c386d159952809
Minseok Kim <mskim.glenn@gmail.com>
2024-11-28 17:31:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 200 ms, Memory: 18104 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,33 +1,36 @@
 // package algorithm_lecture.linkedList;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringJoiner;
 
-// 요세푸스 문제
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringJoiner sj = new StringJoiner(", ");
         StringBuilder sb = new StringBuilder();
 
-        // 입력
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        // 입력 받기
+        int N = sc.nextInt(); // 총 사람 수
+        int K = sc.nextInt(); // 제거 간격
 
-        // 큐 추가
-        Queue<Integer> queue = new ArrayDeque<>();
+        // 사람들을 리스트에 추가
+        List<Integer> list = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            queue.add(i);
+            list.add(i);
         }
 
-        // 리스트가 빌 때까지 반복
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < K - 1; i++) {
-                queue.add(queue.poll()); // 맨 앞에 있는 요소를 맨 뒤로
-            }
-            sj.add(queue.poll().toString()); // K번째 요소는 append
+        // 요세푸스 순열을 저장할 리스트
+        List<Integer> result = new LinkedList<>();
+
+        int index = 0; // 현재 인덱스
+        while (!list.isEmpty()) {
+            index = (index + K - 1) % list.size(); // K번째 사람의 인덱스
+            sj.add(list.remove(index).toString()); // 제거된 사람 추가
         }
 
-        // 최종 결과 출력
+        // 결과 출력
         sb.append("<").append(sj).append(">");
         System.out.println(sb);
     }
