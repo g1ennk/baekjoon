dbc7e11597520567d1609860b833b450ae533aad
Minseok Kim <mskim.glenn@gmail.com>
2024-12-28 19:14:24 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int total = 0;
+        
+        while (n > 0) {
+            total += n % 10;
+            n /= 10;
+        }
+        
+        return total;
+    }
+}
