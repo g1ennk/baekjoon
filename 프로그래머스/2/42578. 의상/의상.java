3d54e60e82ea7c4844ecd351d556bc25dad26416
Minseok Kim <mskim.glenn@gmail.com>
2024-12-30 09:47:13 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,19 +2,17 @@ import java.util.*;
 
 class Solution {
     public static int solution(String[][] clothes) {
-        //  1. 해시맵 초기화
         HashMap<String, Integer> map = new HashMap<>();
-        for (String[] clothe : clothes) {
-            String clothType = clothe[1];
-            map.put(clothType, map.getOrDefault(clothType, 0) + 1);
+
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
 
-        // 2. 경우의 수 계산하기
-        int combination = 1;
-        for (Integer value : map.values()) {
-            combination *= value + 1; // 아무것도 선택하지 않는 경우르 포함하기 위해
+        int count = 1;
+        for (String key : map.keySet()) {
+            count *= map.get(key) + 1;
         }
 
-        return combination - 1; // 아예 아무것도 입지 않는 경우의 수를 제외하기 위해
+        return count - 1;
     }
 }
