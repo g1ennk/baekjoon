7b08a094fd9f679abdf0b2018819abbb6f0cdd47
Minseok Kim <mskim.glenn@gmail.com>
2025-01-02 16:23:35 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 18560 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -4,27 +4,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    private static int[] queue;
+    private int rear;
+    private int front;
+    private int size;
+
+    public Main(int capacity) {
+        queue = new int[capacity];
+        rear = 0;
+        front = 0;
+        size = 0;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        Deque<Integer> queue = new ArrayDeque<>();
 
         // 1. 명렁어의 개수 입력받기
         int N = Integer.parseInt(br.readLine());
 
         // 2. 명령어 실행
+        Main queue = new Main(N);
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
 
             switch (command) {
                 case "push":
-                    queue.offer(Integer.parseInt(st.nextToken()));
+                    queue.push(Integer.parseInt(st.nextToken()));
                     break;
 
                 case "pop":
-                    sb.append((!queue.isEmpty()) ? String.valueOf(queue.poll()) : "-1").append("\n");
+                    sb.append(queue.pop()).append("\n");
                     break;
 
                 case "size":
@@ -32,20 +45,68 @@ public class Main {
                     break;
 
                 case "empty":
-                    sb.append((queue.isEmpty()) ? "1" : "0").append("\n");
+                    sb.append(queue.empty()).append("\n");
                     break;
 
                 case "front":
-                    sb.append((!queue.isEmpty()) ? queue.peekFirst() : "-1").append("\n");
+                    sb.append(queue.front()).append("\n");
                     break;
 
                 case "back":
-                    sb.append((!queue.isEmpty()) ? queue.peekLast() : "-1").append("\n");
+                    sb.append(queue.back()).append("\n");
             }
         }
-        
+
         // 3. 결과 출력
         bw.write(sb.toString());
         bw.flush();
     }
+
+    public int empty() {
+        return isEmpty() ? 1 : 0;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            throw new IllegalStateException("Queue is full");
+        }
+        queue[rear++] = value;
+        size++;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            return -1;
+        }
+        int value = queue[front++];
+        size--;
+        return value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return queue[front];
+    }
+
+    public int back() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return queue[rear - 1];
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private boolean isFull() {
+        return size == queue.length;
+    }
+
 }
