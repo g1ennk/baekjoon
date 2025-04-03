468333395bd092e7d010099b798457dc2d1df8c4
Minseok Kim <mskim.glenn@gmail.com>
2025-04-03 09:38:32 +0900

    [Bronze III] Title: 최소, 최대, Time: 452 ms, Memory: 88428 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,28 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 1. Get the count of numbers to receive
-        int count = scanner.nextInt();
+        // 1. n 입력받기
+        int n = Integer.parseInt(br.readLine());
 
-        // 2. Receive the numbers and find the maximum and minimum numbers
+        // 2. n개의 수 입력받기
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < count; i++) {
-            int number = scanner.nextInt();
-            if (number > max) {
-                max = number;
-            }
-            if (number < min) {
-                min = number;
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
 
-        // 3. Print the result
-        System.out.println(min + " " + max);
-
-        scanner.close();
+        // 3. 결과 출력
+        System.out.print(min + " " + max);
     }
 }
